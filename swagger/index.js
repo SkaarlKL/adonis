d78f87fa02b009bbbf9e42f24e9963fc111d1f14
SkaarlKL <121052752+SkaarlKL@users.noreply.github.com>
2023-01-27 20:56:51 +0000

    asd

--- swagger/index.js
+++ swagger/index.js
@@ -5,7 +5,6 @@ const express = require('express');
 const swagger = require('swagger-ui-express');
 
 const frontendDocs = require('./data/frontend.json');
-const backendDocs = require('./data/backend.json');
 
 // CONSTANTS
 
@@ -18,7 +17,6 @@ const app = express();
 // ROUTES
 
 app.use('/frontend', swagger.serve, (req, res) => { res.send(swagger.generateHTML(frontendDocs)); });
-app.use('/backend', swagger.serve, (req, res) => { res.send(swagger.generateHTML(backendDocs)); });
 
 // SERVER
 
